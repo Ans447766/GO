58a84e06d30c575cd34b8f9134b72f9f4078a436
Ans ishtiaq <ans446677@gmail.com>
2019-01-23 14:51:54 +0500

    ok

--- connection.php
+++ connection.php
@@ -4,4 +4,10 @@ $username = 'root';
 $password = '';
 $database = '';
 $conn = mysqli_connect($host,$username,$password,$database);
+if(!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+}else{
+    echo "<script>console.log('data base connected  successfully')</script>";
+}
+
 ?>
