871e8ad1bfbe531decf14a5ccf1ecbd093850104
Ans Ishtiaq <ans447766@gmail.com>
2019-02-13 16:04:52 +0500

    ok

--- ErrorHandler.php
+++ ErrorHandler.php
@@ -1,8 +1,16 @@
 <?php
-//error handler function
+//error handler functions
 function customError($errno, $errstr) {
-  echo "<b>Error:</b> [$errno] $errstr<br>";
-  die();
+    echo "<b>Error:</b> [$errno] $errstr<br>";
+    die();
+}
+function shutdownHandler() {
+    $lasterror = error_get_last();
+    $error = "[<b>SHUTDOWN</b>] lvl:" . $lasterror['type'] . " | msg:" . $lasterror['message'] . " | file:" . $lasterror['file'] . " | <b>ln:" . $lasterror['line'] ."</b>";
+
+    
+    echo "<div style='background-color:black;color:white;position:fixed;top:0px;bottom:0px;left:0px;right:0px;width:100%;height:100%;font-family:monaco;'>".$error."</div>";
+    die();
 }
 // 2	     E_WARNING	             Non-fatal run-time errors. Execution of the script is not halted
 // 8	     E_NOTICE	               Run-time notices. The script found something that might be an error, but could also happen when running a script normally
@@ -12,4 +20,5 @@ function customError($errno, $errstr) {
 // 4096    	E_RECOVERABLE_ERROR	   Catchable fatal error. This is like an E_ERROR but can be caught by a user defined handle (see also set_error_handler())
 // 8191    	E_ALL	                 All errors and warnings (E_STRICT became a part of E_ALL in PHP 5.4)
 set_error_handler("customError",E_ALL);
+register_shutdown_function("shutdownHandler");
 ?>
